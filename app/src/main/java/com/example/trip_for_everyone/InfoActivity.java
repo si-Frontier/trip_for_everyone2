0198e68848530bc9691559eb31d17a5dd94a3b18
parksk99 <parksk99@naver.com>
2021-12-21 22:33:35 +0900

    info에서 navigation기능 활성화

--- app/src/main/java/com/example/trip_for_everyone/InfoActivity.java
+++ app/src/main/java/com/example/trip_for_everyone/InfoActivity.java
@@ -1,20 +1,53 @@
 package com.example.trip_for_everyone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class InfoActivity extends AppCompatActivity {
-    ImageButton bookmark;
-    boolean isBookmarked;
+    private ImageButton bookmark;
+    private boolean isBookmarked;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private InfoFragment1 infoFragment1 = new InfoFragment1();
+    private InfoFragment2 infoFragment2 = new InfoFragment2();
+    private InfoFragment3 infoFragment3 = new InfoFragment3();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         isBookmarked = false;
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.info_fragment,infoFragment1).commitAllowingStateLoss();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.info_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();   //여기서 다시 선언해서 써야됨
+                switch(item.getItemId()){
+                    case R.id.infomenu1:
+                        fragmentTransaction.replace(R.id.info_fragment,infoFragment1).commitAllowingStateLoss();
+                        break;
+                    case R.id.infomenu2:
+                        fragmentTransaction.replace(R.id.info_fragment,infoFragment2).commitAllowingStateLoss();
+                        break;
+                    case R.id.infomenu3:
+                        fragmentTransaction.replace(R.id.info_fragment, infoFragment3).commitAllowingStateLoss();
+                        break;
+                }
+                return false;
+            }
+        });
 
         bookmark = findViewById(R.id.info_bookmark);
         bookmark.setOnClickListener(new View.OnClickListener() {
