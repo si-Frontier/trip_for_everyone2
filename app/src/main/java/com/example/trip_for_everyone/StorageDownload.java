49e5f7b3aa439d77ff8c25094d772faad265647b
parksk99 <parksk99@naver.com>
2021-12-21 22:10:36 +0900

    관광지 정보 액티비티 일단 추가

--- app/src/main/java/com/example/trip_for_everyone/StorageDownload.java
+++ app/src/main/java/com/example/trip_for_everyone/StorageDownload.java
@@ -5,9 +5,11 @@ import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.GenericLifecycleObserver;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -22,6 +24,11 @@ public class StorageDownload {
             @Override
             public void onSuccess(Uri uri) {
                 Glide.with(context).load(uri).into(view);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println(e.toString());
             }
         });
     }
