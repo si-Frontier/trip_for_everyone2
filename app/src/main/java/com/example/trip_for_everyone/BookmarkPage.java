303e878ccfac7cedd66fe36f854c94a232d6e9e7
parksk99 <parksk99@naver.com>
2022-01-14 15:42:04 +0900

    마이페이지에서 북마크 불러오기

--- app/src/main/java/com/example/trip_for_everyone/BookmarkPage.java
+++ app/src/main/java/com/example/trip_for_everyone/BookmarkPage.java
@@ -1,14 +1,44 @@
 package com.example.trip_for_everyone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class BookmarkPage extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
+    private String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmark_page);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+
+        mDatabase.child("users").child(uid).child("bookmark").orderByChild("bookmark").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot child : snapshot.getChildren()){
+                    Log.d("bookmark", child.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
