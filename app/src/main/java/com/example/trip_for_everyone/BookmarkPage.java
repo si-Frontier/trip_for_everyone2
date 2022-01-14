a9740fd5c63698231b3f43ac112f625354a40e40
parksk99 <parksk99@naver.com>
2022-01-15 00:28:12 +0900

    사용자가 즐겨찾기한 내용 띄우기 (눌렀을때 info 액티비티 불러오는거 해야됨)

--- app/src/main/java/com/example/trip_for_everyone/BookmarkPage.java
+++ app/src/main/java/com/example/trip_for_everyone/BookmarkPage.java
@@ -2,6 +2,9 @@ package com.example.trip_for_everyone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,31 +17,43 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class BookmarkPage extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private String uid;
+    private RecyclerView mRecyclerView;
+    private BookmarkRecyclerView mAdapter;
+    private ArrayList<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmark_page);
+        list = new ArrayList<>();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         uid = user.getUid();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.bookmark_recycler_view);
+        mAdapter = new BookmarkRecyclerView();
 
         mDatabase.child("users").child(uid).child("bookmark").orderByChild("bookmark").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot child : snapshot.getChildren()){
-                    Log.d("bookmark", child.getValue().toString());
+                    list.add(child.getValue().toString());
                 }
+                mAdapter.setBookmarkList(list);
+                mRecyclerView.setAdapter(mAdapter);
+                mRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
+
+
+
     }
 }
